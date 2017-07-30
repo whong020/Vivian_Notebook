3347237c5dc9127d8368f3493c5f81ed781a4b77
Vivian Shao <vivian@dhcp-128-189-224-194.ubcsecure.wireless.ubc.ca>
2017-07-30 16:19:24 -0700

    late assignment

--- js/q1.js
+++ js/q1.js
@@ -1,16 +1,27 @@
 var twoSum = function(nums, target) {
-    
-    
+    var count,sum,acc1 = 0;
+    while(acc1 < nums.length-1){
+      for(count = acc1+1; count < nums.length; count++){
+        sum = nums[acc1] + nums[count]
+        if(sum == target){
+          return true
+        }
+      }
+      acc1 += 1
+    }
     return false;
 };
+
+
 
 
 /**
  * DESIGN
  * A function which will return true if two numbers within the array is equal
  * to the target
- *  - must be done in O(n) time 
- * 
+ *  - must be done in O(n) time \ O(n) space
+ *  - brute force with O(n^2)
+ *
  * Parameters: array , int
  * The test spec is called with like twoSum([1,2,3,4,5], 10)
  * Returns: Boolean
--- js/q2.js
+++ js/q2.js
@@ -1,5 +1,16 @@
 var threeSum = function(nums, target) {
-    return false
+    var count,sum,acc1 = 0;
+    while(acc1 < nums.length-1){
+      for(count = acc1+1; count < nums.length-1; count++){
+        var count2 = count+1
+        sum = nums[acc1] + nums[count] + nums[count2]
+        if(sum == target){
+          return true
+        }
+      }
+      acc1 += 1
+    }
+  return false;
 };
 
 
@@ -7,8 +18,8 @@ var threeSum = function(nums, target) {
  * DESIGN
  * A function which will return true if three numbers within the array is equal
  * to the target
- *  - must be done in O(n^2) time 
- * 
+ *  - must be done in O(n^2) time
+ *
  * Parameters: array , int
  * The test spec is called with something like threeSum([1,2,3,4,5], 10)
  * Returns: Boolean
